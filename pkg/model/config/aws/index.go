0edecb6138fc8b1d26ebf453515eea0451854480
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-07-31 18:08:55 +0200

    feat: deprecate repository

--- pkg/model/config/aws/index.go
+++
@@ -1,7 +0,0 @@
-package aws
-
-// Config defines AWS-related configuration.
-type Config struct {
-	// Postgres contains configuration for PostgreSQL RDS instances.
-	Postgres *RDSConfig `yaml:"postgres,omitempty"`
-}
